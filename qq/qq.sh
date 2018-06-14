ac29ab9ae4a5bee917ed07d5c0174b0a3ed96501
Kijong Yi <kijong.yi@kaist.ac.kr>
2018-06-14 20:56:51 +0900

    .

--- qq/qq.sh
+++ qq/qq.sh
@@ -18,10 +18,12 @@ ssh node0 -x -p 2030 top -b -n 1 > ~/.cpu/0 &
 for i in 1 2 3 4 5 6 7 8; do
     ssh node$i -x top -b n 1 > ~/.cpu/$i &
 done
+
+qs=$(ssh 10.0.2.254 -p 2030 qstat -u $WHO | awk 'BEGIN { R = 0; H = 0; Q = 0 } $10 == "R" { R += 1  } $10 == "H" { H += 1 } $10 == "Q" { Q += 1 } END { print R "R " H "H " Q "Q" }'&)
 wait
 
 rm -rf ~/.cpu/j*
-qstat -n1 -u $WHO |\
+ssh 10.0.2.254 -p 2030 qstat -n1 -u $WHO |\
     sed '1,5d; s/\/.*//;s/.bnode0.kaist.ac.//; s/bnode\([0-9]\)$/~\/.cpu\/j\1/' |\
     awk '$10=="R" {print "echo " $4 " \\\(" $1 "\\\) >> " $12}' 2>/dev/null | bash
 C=( 4 24 24 24 24 24 28 28 28 )
@@ -41,8 +43,8 @@ for i in {0..8}; do
     S=$ALLCOLOR; N=$(($T-$M)); PRINT;
     S=$BACKGROUND; N=$(($L-$T)); PRINT;
     if [ "$i" == "0" ]; then
-		echo -en "\t\t\t\t"
-		qstat -u $WHO | awk 'BEGIN { R = 0; H = 0; Q = 0 } $10 == "R" { R += 1  } $10 == "H" { H += 1 } $10 == "Q" { Q += 1 } END { print R "R " H "H " Q "Q" }'
+		echo -e "\t\t\t\t$qs"
+		#ssh 10.0.2.254 -p 2030 qstat -u $WHO | awk 'BEGIN { R = 0; H = 0; Q = 0 } $10 == "R" { R += 1  } $10 == "H" { H += 1 } $10 == "Q" { Q += 1 } END { print R "R " H "H " Q "Q" }'
     elif [ -f ~/.cpu/j$i ]; then 
 		nj=$(wc -l ~/.cpu/j$i | cut -f1 -d ' ')
 		if [ $nj -gt 1 ]; then njt="+"$(($nj - 1)); fi
